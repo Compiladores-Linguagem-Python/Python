bfc01b80005fdf84647ff4f88c2785bab35d957c
Cindy Vitória <134222487+syndiy@users.noreply.github.com>
2025-10-08 23:43:37 -0300

    Update apresentação3.py

--- Exemplos/apresentação3.py
+++ Exemplos/apresentação3.py
@@ -1,5 +1,74 @@
 # Exemplos de código utilizados na terceira apresentação da linguagem Python.
 
+# DECLARAÇÃO E CHAMADA
+def saudacao(nome):
+    print("Ola,", nome)
+saudacao("Cindy")
+
+def soma(a, b):
+    return a + b
+
+# PASSAGEM DE PARÂMETROS POR VALOR
+def alterar(x):
+    x = 10
+    print("Dentro da função:", x)
+a = 5
+alterar(a)
+print("Fora da função:", a)
+
+# PASSAGEM DE PARÂMETROS POR REFERÊNCIA
+def adicionar(lista):
+    lista.append(4)
+nums = [1, 2, 3]
+adicionar(nums)
+print(nums)
+
+# PASSAGEM DE PARÂMETROS POR VALOR-RESULTADO
+procedure soma(var x: integer; y: integer); begin  
+x := x + y; end;
+
+# PASSAGEM DE PARÂMETROS POR NOME
+procedure imprime_duas_vezes(x)  
+begin  
+    print(x);  
+    print(x);  
+end;  
+integer a := 5;  
+imprime_duas_vezes(a + 1);
+
+# CO-ROTINAS
+def contagem():
+    for i in range(3):
+        yield i
+for numero in contagem():
+    print(numero)
+
+# SUBPROGRAMAS DELEGADOS
+def saudacao():
+    print("Oi, pessoal!")
+mensagem = saudacao
+mensagem()
+
+# FUNÇÕES LAMBDA
+dobrar = lambda x: x * 2
+print(dobrar(5))
+
+# PROGRAMAÇÃO GENÉRICA
+def mostrar(item):
+    print(item)
+mostrar(10)
+mostrar("Cindy")
+mostrar([1,2,3])
+
+# DELEGAÇÃO DE EXCEÇÃO
+def dividir(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Divisão por zero!")
+    return a / b
+try:
+    dividir(5, 0)
+except ZeroDivisionError as e:
+    print("Erro tratado:", e)
 
 # -------------- TRATAMENTO DE EVENTOS E EXCEÇÕES -------------- #
 
